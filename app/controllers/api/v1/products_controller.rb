149f60676f1a10279cdd420599723146e0bca81e
Kmilo1488 <camilomoreno1488@live.com>
2017-11-09 11:20:14 -0500

    Create products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,6 @@ module Api
 	module V1
 		class ProductsController < ApplicationController
 			protect_from_forgery with: :null_session
-			before_action :set_product, only: [:update, :destroy]
 
 			def index
 				render json: Product.all
@@ -11,30 +10,13 @@ module Api
 			def create
 				product = Product.new(product_params)
 				if product.save
-					render json: product, status: :created #201
+					render json: product, status: :created
 				else
-					render json: {errors: product.errors}, status: :unprocessable_entity #422
+					render json: {errors: product.errors}, status: :unprocessable_entity
 				end
-			end
-
-			def update
-				if @product.update(product_params)
-					render json: @product, status: :ok #200
-				else
-					render json: { errors: @product.errors }, status: :unprocessable_entity #422
-				end
-			end
-
-			def destroy
-				@product.destroy
-				head :no_content #204
 			end
 
 			private
-				def set_product
-      		@product = Product.find(params[:id])
-    		end
-
 				def product_params
 					params.require(:product).permit(:name, :price)
 				end
