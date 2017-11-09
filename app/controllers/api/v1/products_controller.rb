95dafeee7c0aab51bb62f2945f56607a4b049b62
Kmilo1488 <camilomoreno1488@live.com>
2017-11-09 11:24:57 -0500

    Edit-delete products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,6 +3,8 @@ module Api
 		class ProductsController < ApplicationController
 			protect_from_forgery with: :null_session
 
+			before_action :set_product, only: [:update, :destroy]
+
 			def index
 				render json: Product.all
 			end
@@ -10,13 +12,30 @@ module Api
 			def create
 				product = Product.new(product_params)
 				if product.save
-					render json: product, status: :created
+					render json: product, status: :created #201
 				else
-					render json: {errors: product.errors}, status: :unprocessable_entity
+					render json: {errors: product.errors}, status: :unprocessable_entity #422
 				end
 			end
 
+			def update
+				if @product.update(product_params)
+					render json: @product, status: :ok #200
+				else
+					render json: { errors: @product.errors }, status: :unprocessable_entity #422
+				end
+			end
+
+			def destroy
+				@product.destroy
+				head :no_content #204
+			end
+
 			private
+				def set_product
+      		@product = Product.find(params[:id])
+    		end
+
 				def product_params
 					params.require(:product).permit(:name, :price)
 				end
